501891d16b0072bd3eb6c2ec2305b7ed0d27e956
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2024-08-22 14:33:58 +0530

    up

---
+++ datatype.php
@@ -0,0 +1,64 @@
+<?php
+//write the php script to calculate the percentage
+// function calculatePercentage($totalMarks, $obtainedMarks) {
+//     return ($obtainedMarks / $totalMarks) * 100;
+// }
+
+// function determineGrade($percentage) {
+//     if ($percentage >= 90) {
+//         return "A+";
+//     } elseif ($percentage >= 80) {
+//         return "A";
+//     } elseif ($percentage >= 70) {
+//         return "B";
+//     } elseif ($percentage >= 60) {
+//         return "C";
+//     } elseif ($percentage >= 50) {
+//         return "D";
+//     } else {
+//         return "F";
+//     }
+// }
+// $totalMarks = 500; 
+// $obtainedMarks = 425; 
+// $percentage = calculatePercentage($totalMarks, $obtainedMarks);
+// $grade = determineGrade($percentage);
+// echo "Percentage: " . $percentage . "%<br>";
+// echo "Grade: " . $grade;
+
+
+//calculate the area of circle,square,rectangle and traingle
+
+// function areaOfCircle($radius) {
+//     return pi() * pow($radius, 2);
+// }
+// function areaOfSquare($side) {
+//     return pow($side, 2);
+// }
+// function areaOfRectangle($length, $width) {
+//     return $length * $width;
+// }
+// function areaOfTriangle($base, $height) {
+//     return 0.5 * $base * $height;
+// }
+// $radius = 5;
+// $circleArea = areaOfCircle($radius);
+// echo "Area of the circle: " . $circleArea . " square units<br>";
+
+// $side = 4;
+// $squareArea = areaOfSquare($side);
+// echo "Area of the square: " . $squareArea . " square units<br>";
+
+// $length = 6;
+// $width = 3;
+// $rectangleArea = areaOfRectangle($length, $width);
+// echo "Area of the rectangle: " . $rectangleArea . " square units<br>";
+
+// $base = 8;
+// $height = 5;
+// $triangleArea = areaOfTriangle($base, $height);
+// echo "Area of the triangle: " . $triangleArea . " square units<br>";
+
+
+
+?>
---
+++ hello.php
@@ -0,0 +1,29 @@
+<?php
+//my first php script
+//php is loosely typed language
+echo "hello!" ;
+
+//php script add two numbers
+$x=10;
+$y=20;
+echo "<br>";
+echo "the addition of two numbers is ". $x+$y. "<br>" ;
+echo "the subtraction of two numbers is ". $x-$y. "<br>" ;
+echo "the multiplication of two numbers is ". $x*$y. "<br>" ;
+echo "the division of two numbers is ". $x/$y. "<br>";
+echo "the remainder of two numbers is ". $x%$y. "<br> ";
+
+$a=8;
+$b=6.8;
+$c="String";
+$d="null";
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+
+
+
+?>
+
+<?="<br> hello";?>
