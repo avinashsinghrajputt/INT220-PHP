a290f1cf21ec073da12e889f767ea30e5e48143f
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2024-10-02 14:15:37 +0530

    up

--- const.php
+++ const.php
@@ -1,21 +1,31 @@
 <?php
 const X = 45;
 const Y = 78;
-    
+
 function add() {
     $res = X + Y;
-    echo "the sum of number: ", $res ,"<br>";
+    echo "The sum of the numbers: ", $res ,"<br>";
 }
 
 add();
-?>
 
-
-<?php
 $num = 7;
 $result = ($num % 2 == 0) ? "even" : "odd";
-echo "The number $num is $result.";
+echo "The number $num is $result.<br>";
+
+function firstNonNull(...$values) {
+    foreach ($values as $value) {
+        if ($value !== null) {
+            return $value;
+        }
+    }
+    return null;
+}
+
+$val1 = null;
+$val2 = null;
+$val3 = 5;
+$val4 = 10;
+
+echo "The first non-null value is: ", firstNonNull($val1, $val2, $val3, $val4);
 ?>
-
-
-print first non null val using
