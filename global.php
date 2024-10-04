7c3afb69d9054e3b0067738b5a6035f17ccb30cb
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2024-10-04 15:36:51 +0530

    up

--- global.php
+++ global.php
@@ -1,14 +1,30 @@
 <?php
 
-$x =5;
-$y=10;
+$x = 5;
+$y = 10;
 
-function add(){
-    //
-    global $x,$y;
-    return $x+$y;
+function add() {
+    global $x, $y;
+    return $x + $y;
 }
-$result= add();
-echo $result;
+
+function multiply() {
+    global $x, $y;
+    return $x * $y;
+}
+
+function findMax() {
+    global $x, $y;
+    return max($x, $y);
+}
+
+$result = add();
+echo "Sum: " . $result . "<br>";
+
+$result = multiply();
+echo "Product: " . $result . "<br>";
+
+$result = findMax();
+echo "Maximum: " . $result . "<br>";
 
 ?>
